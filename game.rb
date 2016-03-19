03555a35a820fb8bcce63c42c1bcb7ef3037d16e
Andrew Kampjes <a.kampjes@gmail.com>
2016-03-20 12:36:24 +1300

    Add all the assets and write out progress

--- game.rb
+++ game.rb
@@ -1,30 +1,29 @@
 require 'gosu'
-#require_relative './z_order'
 
 require_relative 'hang_man'
 require_relative 'hangman_view'
 require_relative 'random_word'
 
 class GameWindow < Gosu::Window
+  GAME_TURNS = 8
+
   def initialize
-    super 640, 480
+    super 1920, 1200
     self.caption = "Hangman :P"
 
-    #@background_image = Gosu::Image.new("media/space.png", :tileable => true)
-
-    #@player = Player.new
-    #@player.warp(320, 240)
-
-    #@star_anim = Gosu::Image::load_tiles("media/star.png", 25, 25)
-    #@stars = Array.new
-
     random_word = RandomWord.new('words.txt').get_word
-    #@hangman = HangMan.new(random_word)
-    @hangman = HangMan.new("hang")
-    @progress = Gosu::Font.new(20)
-    @tried_letters = Gosu::Font.new(20)
-    @remaining_turns = Gosu::Font.new(20)
-    @game_message = Gosu::Font.new(20)
+    random_word = "hang"
+    @hangman = HangMan.new("hang", turns: GAME_TURNS)
+
+    @background_image = Gosu::Image.new("media/windows-xp-background.jpg")
+    @stickfigure = Gosu::Image.new("media/stickfigure288px.png")
+    @balloons = []
+    @balloon = Gosu::Image.new("media/balloon.png")
+
+    @progress = Gosu::Font.new(50)
+    @tried_letters = Gosu::Font.new(50)
+    @remaining_turns = Gosu::Font.new(50)
+    @game_message = Gosu::Font.new(50)
 
     @message = ""
   end
@@ -38,12 +37,16 @@ class GameWindow < Gosu::Window
   end
 
   def draw
-    #@background_image.draw(0, 0, ZOrder::Background)
-    @progress.draw(@hangman.word_progress, 10, 10, 1, 1.0, 1.0, 0xff_ffff00)
+    @background_image.draw(0, 0, 0)
+    @stickfigure.draw(800, 600, 1)
+
+    @balloon.draw(800, 300, 1)
+
+    @progress.draw(@hangman.word_progress, 10, 10, 1, 1.0, 1.0, 0xff_000000)
     # could probs exclude letters that were correct
-    @tried_letters.draw("Tried letters: #{@hangman.tried_letters.join(',')}", 10, 50, 1, 1.0, 1.0, 0xff_ffff00)
-    @game_message.draw(@message, 10, 200, 1, 1.0, 1.0, 0xff_ffff00)
-    @progress.draw("Remaining turns #{@hangman.remaining_turns}", 10, 100, 1, 1.0, 1.0, 0xff_ffff00)
+    @tried_letters.draw("Tried letters: #{@hangman.tried_letters.join(',')}", 10, 100, 1, 1.0, 1.0, 0xff_000000)
+    @game_message.draw(@message, 10, 200, 1, 1.0, 1.0, 0xff_000000)
+    @progress.draw("Remaining turns #{@hangman.remaining_turns}", 10, 300, 1, 1.0, 1.0, 0xff_000000)
   end
 
   def button_down(id)
@@ -62,7 +65,6 @@ class GameWindow < Gosu::Window
         rescue HangMan::AlreadyUsedLetterError => e
           @message = "You've already tried that letter silly :P"
         end
-
       end
     end
   end
